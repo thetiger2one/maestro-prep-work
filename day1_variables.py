b01bc011a918657df526f2a108335a581e6bf17a
Tony Miranda <thetiger2one@yahoo.com>
2025-08-22 07:59:10 -0700

    feat: first project added to repository

---
+++ day1_variables.py
@@ -0,0 +1,70 @@
+my_name = "Tony"
+print(my_name)
+# Now for an integer (a whole number)
+my_age = 38
+print(my_age)
+
+# And a float (a number with a decimal)
+project_version = 1.0
+print(project_version)
+
+# Finally, a boolean (True or False)
+is_mission_go = True
+print(is_mission_go)PS C:\Users\Tony\Desktop\PythonProjects> & C:/Users/Tony/AppData/Local/Programs/Python/Python313/python.exe c:/Users/Tony/Documents/PythonLearning/day1_variables.py
+  File "c:\Users\Tony\Documents\PythonLearning\day1_variables.py", line 14
+    git clone https://github.com/thetiger2one/maestro-prep-work.git
+        ^^^^^      
+SyntaxError: invalid syntax
+PS C:\Users\Tony\Desktop\PythonProjects> PS C:\Users\Tony\Desktop\PythonProjects> & C:/Users/Tony/AppData/Local/Programs/Python/Python313/python.exe c:/Users/Tony/Documents/PythonLearning/day1_variables.py    
+>>   File "c:\Users\Tony\Documents\PythonLearning\day1_variables.py", line 14                  
+>>     git clone https://github.com/thetiger2one/maestro-prep-work.git      
+>>         ^^^^^                      
+>> SyntaxError: invalid syntax        
+>> PS C:\Users\Tony\Desktop\PythonProjects>              
+PS C:\Users\Tony\Desktop\PythonProjects> PS C:\Users\Tony\Desktop\PythonProjects> & C:/Users/Tony/AppData/Local/Programs/Python/Python313/python.exe c:/Users/Tony/Documents/PythonLearning/day1_variables.py    
+>>   File "c:\Users\Tony\Documents\PythonLearning\day1_variables.py", line 14
+>>     git clone https://github.com/thetiger2one/maestro-prep-work.git      
+>>         ^^^^^   
+
+>> SyntaxError: invalid syntax        
+>> PS C:\Users\Tony\Desktop\PythonProjects>
+At line:1 char:42
++ PS C:\Users\Tony 
+\Desktop\PythonPro 
+jects> & C:/Users/ 
+Tony/AppData/Loca  
+...
++
+
+       ~
+The ampersand (&)  
+character is not   
+allowed. The &     
+operator is        
+reserved for       
+future use; wrap   
+an ampersand in    
+double quotation   
+marks ("&") to     
+pass it as part    
+of a string.       
+    + CategoryInf  
+   o          :    
+  ParserError:     
+ (:) [], Paren     
+tContainsErro      
+rRecordExcept      
+ion
+    + FullyQualif  
+   iedErrorId :    
+  AmpersandNotA    
+ llowed
+
+PS C:\Users\Tony\Desktop\PythonProjects> cd C:\Users\Tony\Documents
+PS C:\Users\Tony\Documents> git clone https://github.com/thetiger2one/maestro-prep-work.git    
+Cloning into 'maestro-prep-work'...   
+remote: Enumerating objects: 3, done. 
+remote: Counting obremote: Counting obremote: Counting obremote: Counting objects: 100% (3/3), done.
+remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)       
+Receiving objects: Receiving objects: Receiving objects: Receiving objects: 100% (3/3), done.  
+PS C:\Users\Tony\Documents>
